da24fa45a9e4a76714a5cf023ecd89a882337850
Yuval Chay Chabra <109518434+yuvch98@users.noreply.github.com>
2024-01-14 21:04:08 +0200

    second commit

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
-# importing libraries
-import numpy as np
+#importing files and libraries
 import cv2
-import matplotlib as plt
+import numpy as np
+# from google.colab.patches import cv2_imshow -> this is only to be used when using google colab!
+from matplotlib import pyplot as plt
+from tensorflow import Keras
+
+#frame - has 240x320 pixels in our current example
+#pixel - (0-255,0-255,0-255)
+#we receive the frame already in a shape of (240,320,3)
+#we need to flatten it in order to put it in a model my_instance.reshape(amount_of_frames, 240*320*3)
+
+
+# the following code is how we capture a video using cv2 library
+
+cap = cv2.VideoCapture("Abuse001_x264.mp4")
+while True:
+
+    ret, frame = cap.read()
+    if ret is False:
+        break
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1)
+    if key == 27:  # if pressed on escape
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+label = [0, 1]  # normal / anomaly
